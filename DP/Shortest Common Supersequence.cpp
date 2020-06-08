3031bd6d9749cf8bf49c863ede2ce76236b657d4
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2020-06-09 02:45:58 +0530

    Create Shortest Common Supersequence.cpp

---
+++ DP/Shortest Common Supersequence.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int LCS(string s1, string s2, int m, int n,vector<vector<int>>&dp){
+    for(int i =  0; i <=n; i++){
+        dp[0][i] = 0;
+    }
+    for(int i = 0; i<=m; i++){
+        dp[i][0] = 0;
+    }
+    for(int i = 1; i <= m; i++){
+        for(int j = 1; j <= n;j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main() {
+    int t;
+    int m,n;
+    string s1,s2;
+    cin>>t;
+    while(t--){
+        cin>>s1>>s2;
+        m = s1.length();
+        n = s2.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1));
+        cout<<(m+n-LCS(s1,s2,m,n,dp))<<endl;
+    }
+	return 0;
+}
