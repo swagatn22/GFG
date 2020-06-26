98bd6f725821771469dfbddfc52fc61db34b65e5
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2020-06-26 18:44:25 +0530

    Update Unit Area of largest region of 1's .cpp

--- Graph/Unit Area of largest region of 1's .cpp
+++ Graph/Unit Area of largest region of 1's .cpp
@@ -1,18 +1,17 @@
-void dfs(int A[SIZE][SIZE],int i,int j,int N, int M,int &area,int &ans){
+void dfs(int A[SIZE][SIZE],int i,int j,int N, int M,int &area){
     if(i < 0 || j < 0 || i >= N || j >= M || A[i][j] != 1){
         return;
     }
     area += A[i][j];
-    ans = max(ans,area);
     A[i][j] = -1;
-    dfs(A,i+1,j,N,M,area,ans);
-    dfs(A,i-1,j,N,M,area,ans);
-    dfs(A,i,j+1,N,M,area,ans);
-    dfs(A,i,j-1,N,M,area,ans);
-    dfs(A,i-1,j-1,N,M,area,ans);
-    dfs(A,i-1,j+1,N,M,area,ans);
-    dfs(A,i+1,j+1,N,M,area,ans);
-    dfs(A,i+1,j-1,N,M,area,ans);
+    dfs(A,i+1,j,N,M,area);
+    dfs(A,i-1,j,N,M,area);
+    dfs(A,i,j+1,N,M,area);
+    dfs(A,i,j-1,N,M,area);
+    dfs(A,i-1,j-1,N,M,area);
+    dfs(A,i-1,j+1,N,M,area);
+    dfs(A,i+1,j+1,N,M,area);
+    dfs(A,i+1,j-1,N,M,area);
 }
 int findMaxArea(int N, int M, int A[SIZE][SIZE] )
 {
@@ -22,7 +21,8 @@ int findMaxArea(int N, int M, int A[SIZE][SIZE] )
        for(int j = 0; j < M; j++){
           area = 0;
           if(A[i][j] == 1){
-              dfs(A,i,j,N,M,area,ans);
+              dfs(A,i,j,N,M,area);
+              ans = max(ans,area);
           }
        }
    }
