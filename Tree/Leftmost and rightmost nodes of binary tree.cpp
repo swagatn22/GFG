75f5f6bcbb1b3528ef580c5ef6312d00ecb5708b
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2020-06-28 20:34:05 +0530

    Create Leftmost and rightmost nodes of binary tree.cpp

---
+++ Tree/Leftmost and rightmost nodes of binary tree.cpp
@@ -0,0 +1,22 @@
+void printCorner(Node *root)
+{
+    if(root==NULL) return ;
+    
+    queue<Node*> q;
+    q.push(root);
+    int p=0;
+    while(!q.empty()) {
+        p = q.size();
+        for(int i = 1;i <= p; i++) {
+            root = q.front();
+            q.pop();
+            if(i==1 || i==p) {
+                cout<<root->data<<" ";
+            }
+            if(root->left)
+                q.push(root->left);
+            if(root->right)
+                q.push(root->right);
+        }
+    }
+}
