b4bbd5fbee13c9409ed5f7f92842e86e45277427
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2020-07-10 19:43:57 +0530

    Create Two Mirror Trees.cpp

---
+++ Tree/Two Mirror Trees.cpp
@@ -0,0 +1,9 @@
+int areMirror(Node* a, Node* b) {
+    if(a == NULL &&  b == NULL){
+        return 1;
+    }
+    if(a == NULL || b == NULL){
+        return 0;
+    }
+    return(a->data == b->data && areMirror(a->left,b->right) && areMirror(a->right,b->left));
+}
