b43cd78c9e1fe2188a816c5cc8f017a043368e16
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2021-05-11 21:13:00 +0530

    Update Implementing Dijkstra Algorithm .cpp

--- Graph/Implementing Dijkstra Algorithm .cpp
+++ Graph/Implementing Dijkstra Algorithm .cpp
@@ -25,3 +25,25 @@ vector <int> dijkstra(vector<vector<int>> g, int src, int V)
     }
     return res;
 }
+//adjency list
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        vector<int>dis(V,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        dis[S] = 0;
+        pq.push({0,S});
+        while(!pq.empty()){
+            int u = pq.top().second;
+            int d = pq.top().first;
+            pq.pop();
+            for(int i = 0; i < adj[u].size(); i++){
+                if(dis[adj[u][i][0]] > adj[u][i][1] + d){
+                    dis[adj[u][i][0]] = adj[u][i][1]+d;
+                    pq.push({dis[adj[u][i][0]],adj[u][i][0]});
+                }
+            }
+        }
+        return dis;
+        // Code here
+    }
+};
