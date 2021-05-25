167990168dd335a8f7b1f3fb79f6508935ac7890
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2021-05-25 19:50:37 +0530

    Update ToggleBitsInGivenRange.cpp

--- BIt_manipulation/ToggleBitsInGivenRange.cpp
+++ BIt_manipulation/ToggleBitsInGivenRange.cpp
@@ -32,6 +32,13 @@ int solve(int num, int s, int e){
     }
     return num;
 }
+int toggleBits(int N , int L , int R) {
+        // code here
+        for(int i = L-1; i < R; i++){
+            N = N ^ (1<<i);
+        }
+        return N;
+    }
 
 int main() {
 	//code
